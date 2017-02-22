a6410a810512b1ff0444df868258f99455ddcbc1
Michael Mizner <michaelmizner@gmail.com>
2017-02-22 10:14:47 -0500

    Refactored Featurette

--- components/featurette.php
+++ components/featurette.php
@@ -1,29 +1,38 @@
+<?php
+$prefix      = 'featurette_';
+$title       = get_field( $prefix . 'title' );
+$subtitle    = get_field( $prefix . 'sub_title' );
+$button_link = get_field( $prefix . 'button_link' );
+$button_text = get_field( $prefix . 'button_text' );
+$option      = get_field( $prefix . 'asset_option' );
+$image       = get_field( $prefix . 'image' );
+$image_url   = $image['url'];
+$image_alt   = $image['alt'];
+$video       = get_field( $prefix . 'video' );
+$icon        = get_field( $prefix . 'icon' );
+?>
+
 <section class="featurette">
     <div class="container">
         <div class="box">
             <header>
-                <h3> <?php the_field( 'featurette_title' ); ?></h3>
-				<?php the_field( 'featurette_sub_title' ); ?>
-                <a href="<?php the_field( 'featurette_button_link' ); ?>">
-                    <button class="button"><?php the_field( 'featurette_button_text' ); ?></button>
+                <h3> <?php echo $title; ?></h3>
+				<?php echo $subtitle; ?>
+                <a href="<?php echo $button_link ?>">
+                    <button class="button"><?php echo $button_text; ?></button>
                 </a>
             </header>
             <aside>
-				<?php $option = get_field( 'featurette_asset_option' ); ?>
-				<?php if ( $option == 'image' ):
-					var_dump( $option );
-					?>
-					<?php $image = get_field( 'featurette_image' ); ?>
+				<?php if ( $option == 'image' ): ?>
                     <div class="object-fit-fix">
-                        <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
+                        <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
                     </div>
 				<?php elseif ( $option == 'video' ): ?>
                     <div class="embed-container">
-						<?php the_field( 'featurette_video' ); ?>
+						<?php echo $video; ?>
                     </div>
 				<?php elseif ( $option == 'icon' ): ?>
-					<?php the_field( 'featurette_icon' ); ?>
-				<?php else: ?>
+					<?php echo $icon; ?>
 				<?php endif; ?>
             </aside>
         </div>
